46c9b7bf26c5d2b232a6bf99d51b26a53cc27b5b
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2022-11-17 23:17:46 +0100

    first commit

--- CyBox.Data/Entities/User.cs
+++ CyBox.Data/Entities/User.cs
@@ -9,9 +9,9 @@ namespace CyBox.Data.Entities
     public class User
     {
         public int UserId { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
     }
 }
