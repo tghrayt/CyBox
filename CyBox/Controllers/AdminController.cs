db142b93515f57c58cb99c494f5fad14eef1bf9e
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2022-11-19 16:22:11 +0100

    Home page implementation

--- CyBox/Controllers/AdminController.cs
+++ CyBox/Controllers/AdminController.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace CyBox.Controllers
 {
-    public class CyBoxController : Controller
+    public class AdminController : Controller
     {
         public IActionResult Index()
         {
-            DateTime now = DateTime.Now;
-            ViewData["date"] = now.Year;
             return View();
         }
     }
