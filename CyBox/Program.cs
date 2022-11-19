1712f39978061c973f28acbb392bddd38c818dda
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2022-11-19 01:15:51 +0100

    homepage development design

--- CyBox/Program.cs
+++ CyBox/Program.cs
@@ -1,28 +1,14 @@
+using CyBox;
 using CyBox.Data.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var startup = new Startup(builder.Configuration);
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<CyBoxDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("StoreConnStr")));
 
+startup.ConfigureServices(builder.Services); // calling ConfigureServices method
 var app = builder.Build();
+startup.Configure(app, builder.Environment); // calling Configure method
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
